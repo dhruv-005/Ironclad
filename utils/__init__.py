360020e882ce19287f369cf0b23cdfe0a98886b3
dhruv-005 <dhruvsonani3312@gmail.com>
2026-05-27 12:13:12 +0530

    🔧 utils: Initialize utilities module

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from utils.face_utils   import load_face_model, extract_embedding, validate_image
+from utils.osint_utils  import gather_osint, format_osint_results, ddg_search
+from utils.report_utils import generate_dossier, save_report, assess_risk_level
